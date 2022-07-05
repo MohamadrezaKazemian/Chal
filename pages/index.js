365178a62ebaf6a47714dd6db84924fb7b450797
fffarzan <far.ned.me@gmail.com>
2022-07-05 19:55:54 +0430

    some clean code works

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import {useState, useRef, useEffect} from "react";
+import { useState, useRef, useEffect } from "react";
+import { browserName } from "react-device-detect";
 
-import {Button, Input, Form, Modal} from 'antd';
+import { Button, Input, Form, Modal } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+import { motion } from "framer-motion"
+import { PrismaClient } from '@prisma/client';
+
 import style from "../style/index.module.scss"
 import 'antd/dist/antd.css';
-import React from 'react'
-import {motion} from "framer-motion"
-import {PrismaClient} from '@prisma/client';
-import {CloseOutlined} from '@ant-design/icons';
-import {browserName} from "react-device-detect";
 
 const prisma = new PrismaClient();
-
 
 export async function getServerSideProps() {
     const userMessages = await prisma.user.findMany();
@@ -47,11 +46,10 @@ async function deleteMessage(messageContent , setContent , index , content) {
 
 export default function Index ({initialMessages}) {
     const [content, setContent] = useState(initialMessages)
-    console.log(content)
     const input = useRef(null)
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [inputValue, setInputValue] = useState("")
-    // handlers
+
     const handleSubmitText = async function () {
         await savedMessage(
             {
@@ -80,38 +78,39 @@ export default function Index ({initialMessages}) {
     useEffect(() => {
         // setTimeout(showModal , 10000)
     }, [])
-    return (
 
+    return (
         <div className={style.chatRoom}>
             <motion.path
                 animate={{pathLength: 1}}
                 transition={{duration: 2, type: "tween"}}
                 className={style.messages}
-            >         {
-                content.length < 1 ?
-                    <div className={style.firstTime}>
+            >      
+                {content.length < 1 ?
+                    <section className={style.firstTime}>
                         <h1 className={style.preTitle}>برنامه هات رو اینجا بنویس : )</h1>
                         <div className={style.arrow}>
                             <h2>از اینجا میتونی اولین برنامت رو بنویسی!</h2>
                             <img src={"http://assets.stickpng.com/images/58f8bcf70ed2bdaf7c128307.png"}/>
                         </div>
-                    </div>
+                    </section>
                     :
-                    content.map((item, index) => {
-                        return (
-                            <span  id={index + 1}
-                                  key={index}>{item.message ? item.message : item.toString()}
-                                <div className={style.deleteIcon}>
-                                    <span onClick={()=>{
-                                        deleteMessage(item.message , setContent , index , content)
-                                    }}><CloseOutlined /></span>
-                                </div>
-                            </span>
-                        )
-                    })
-
-            }
+                    content.map((item, index) => (
+                        <span 
+                            id={index + 1}
+                            key={index}
+                        >
+                            {item.message ? item.message : item.toString()}
+                            <div className={style.deleteIcon}>
+                                <span onClick={()=>{deleteMessage(item.message , setContent , index , content)}}>
+                                    <CloseOutlined />
+                                </span>
+                            </div>
+                        </span>
+                    ))
+                }
             </motion.path>
+
             <Form
                 name="basic"
                 labelCol={{span: 8}}
@@ -121,22 +120,36 @@ export default function Index ({initialMessages}) {
                 autoComplete="off"
                 className={style.form}
             >
-                <Input ref={input} required={true} onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
+                <Input 
+                    ref={input} 
+                    required={true} 
+                    onChange={(e) => setInputValue(e.target.value)} 
+                    value={inputValue}
+                />
                 <Button htmlType="submit" type="primary">ارسال</Button>
             </Form>
-            <Modal title="Support!" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}
-                   forceRender={false}>
-                <div className={style.modal}>
+
+            <Modal 
+                title="Support!" 
+                visible={isModalVisible} 
+                onOk={handleOk} 
+                onCancel={handleCancel} 
+                footer={null}
+                forceRender={false}
+            >
+                <section className={style.modal}>
                     <h1>Support us!</h1>
                     <div>
                         <a href="google.com">
-                            <img width={100} src={"https://rmhccnaz.org/wp-content/uploads/Donate-Icon-01.png"}/>
+                            <img 
+                                width={100} 
+                                src={"https://rmhccnaz.org/wp-content/uploads/Donate-Icon-01.png"}
+                            />
                         </a>
                     </div>
-                </div>
+                </section>
             </Modal>
         </div>
-
     )
 }
 
